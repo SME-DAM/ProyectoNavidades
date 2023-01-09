dec0c5b6c42b301e73104ada6196996d0e33e6fc
Ryutaro <ruytaro@theseed.games>
2023-01-09 17:16:46 +0100

    añadidos más comentarios

--- src/Samuel_Martin_Candy.java
+++ src/Samuel_Martin_Candy.java
@@ -1,7 +1,7 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-// 20 horas de trabajo
+// 22 horas de trabajo
 
 public class Samuel_Martin_Candy {
 
@@ -75,29 +75,27 @@ public class Samuel_Martin_Candy {
 		}
 	}
 
-	// vende el producto y devuelve el valor de la venta
-	// si no hay existencias no hace nada y devuelve 0
+	// descuenta el producto y devuelve el valor de la venta
+	// si no hay existencias no hace nada y devuelve -1
 	static double venderProducto(int[] posicion, int[] ventas, int[][] tienda, int[][] existencias, double[] precios) {
 		int producto = eligeProducto(posicion, tienda);
-		int stock = compruebaExistencias(posicion, existencias);
-		if (stock > 0) {
-			establecerExistencias(posicion, existencias, stock - 1);
-			ventas[producto]++;
-			return precios[producto];
+		if(!actualizarExistencias(posicion, existencias, -1)) {
+			return -1;
 		}
-		return -1;
+		ventas[producto]++;
+		return precios[producto];
 	}
 
 	// muestra los productos de cada casilla con su precio
 	static String mostarProductos(int[][] tienda, String[] productos, double[] precios) {
-		String out="";
+		String out = "";
 		int filas = tienda[0].length;
 		int columnas = tienda.length;
 		for (int fila = 0; fila < filas; fila++) {
 			for (int columna = 0; columna < columnas; columna++) {
 				int producto = tienda[fila][columna];
-				out += String.format("| %-20s | Posicion: %c%c | Precio: %.2f |\n", productos[producto],
-						'A' + fila, 'A' + columna, precios[producto]);
+				out += String.format("| %-20s | Posicion: %c%c | Precio: %.2f |\n", productos[producto], 'A' + fila,
+						'A' + columna, precios[producto]);
 			}
 		}
 		return out;
@@ -107,7 +105,7 @@ public class Samuel_Martin_Candy {
 	// sus existencias y sus ventas
 	private static String infoProductos(int[][] tienda, int[][] existencias, String[] productos, double[] precios,
 			int[] ventas) {
-		String out ="";
+		String out = "";
 		for (int prod = 0; prod < 16; prod++) {
 			if (productos[prod].isEmpty())
 				return out;
@@ -119,40 +117,41 @@ public class Samuel_Martin_Candy {
 					}
 				}
 			}
-			out += String.format("| %-20s | Precio: %.2f | U. disponibles: %3d | Ventas: %3d |\n",
-					productos[prod], precios[prod], unidades, ventas[prod]);
+			out += String.format("| %-20s | Precio: %.2f | U. disponibles: %3d | Ventas: %3d |\n", productos[prod],
+					precios[prod], unidades, ventas[prod]);
 		}
 		return out;
 	}
 
 	// pide una posicion al usuario y si es válida la devuelve en un vector
 	// en caso contrario devuelve un vector con valores negativos
-	private static int[] recogePosicion(String entrada) {
-		int[] out = {-1, -1};
-		if (entrada.length() >= 2) {
-			int fila = entrada.charAt(0) - 'A';
-			int columna = entrada.charAt(1) - 'A';
-			if (!(fila < 0 || fila > 3 || columna < 0 || columna > 3)) {
-				out [0] = fila;
-				out[1] = columna;
-				return out;
-			}
+	private static boolean recogePosicion(String entrada, int[] out) {
+		if (entrada.length() != 2) {
+			return false;
 		}
-		return out;
+		int fila = entrada.charAt(0) - 'A';
+		int columna = entrada.charAt(1) - 'A';
+		if ((fila < 0 || fila > 3 || columna < 0 || columna > 3)) {
+			return false;
+		}
+		out[0] = fila;
+		out[1] = columna;
+		return true;
 	}
 
-	// recibe la posicion a rellenar y pide la cantidad a reponer,
-	// si es posible añadirlo devuelve true, en caso contrario false.
+	//verifica los datos de entrada y trata de actualizar la cantidad
+	//devuelve el texto a mostrar al usuario. 
 	private static String reponerProducto(int[] posicion, int[][] existencias, int cantidad) {
-		if (cantidad < 0) return "\nNo puedes reponer una cantidad negativa\n";
-		if (cantidad == 0) return "\n¿Que sentido tiene reponer 0 unidades?\n";
-		int stock = compruebaExistencias(posicion, existencias);
-		if (cantidad + stock <= 5) {
-			establecerExistencias(posicion, existencias, stock + cantidad);
-			return "\nEl producto se ha repuesto correctamente\n\n";
-		} else {
-			return "\nNo caben tantas unidades\n\n";
+		if (cantidad < 0) {
+			return "\nNo puedes reponer una cantidad negativa\n";
 		}
+		if (cantidad == 0) {
+			return "\n¿Qué sentido tiene reponer 0 unidades?\n";
+		}
+		if (!actualizarExistencias(posicion, existencias, cantidad)) {
+			return "\nNo caben tantas unidades\n";
+		}
+		return "\nEl producto se ha repuesto correctamente\n\n";
 	}
 
 	// devuelve las existencias de un slot
@@ -160,18 +159,27 @@ public class Samuel_Martin_Candy {
 		return existencias[posicion[0]][posicion[1]];
 	}
 
+	private static void reseteaExistencias(int[] posicion, int[][] existencias) {
+		existencias[posicion[0]][posicion[1]]=0;
+	}
+	
 	// establece las existencias en un slot
-	private static void establecerExistencias(int[] posicion, int[][] existencias, int stock) {
-		existencias[posicion[0]][posicion[1]] = stock;
+	private static boolean actualizarExistencias(int[] posicion, int[][] existencias, int cantidad) {
+		int stock = compruebaExistencias(posicion, existencias);
+		if (cantidad + stock > 5 || cantidad + stock <= 0) {
+			return false;
+		}
+		existencias[posicion[0]][posicion[1]] += cantidad;
+		return true;
 	}
 
 	// pide el precio en bucle hasta que es >0, despues lo establece en la posicion
 	// indicada
 	private static String actualizarPrecio(int index, double[] precios, Double precio) {
-		if (precio < 0) return "\nNo tiene sentido pagar por que se lleven algo\n";
-		if (precio == 0) return "\nNo soy una ONG, tengo que ganar dinero!\n";
+		if (precio <= 0)
+			return "\nNo soy una ONG, tengo que ganar dinero!\n";
 		precios[index] = precio;
-		return String.format("\nPrecio actualizado a %.2f€\n",precio);
+		return String.format("\nPrecio actualizado a %.2f€\n", precio);
 	}
 
 	// devuelve el indice que contiene ese nombre de producto, o 1 en caso de no
@@ -202,7 +210,8 @@ public class Samuel_Martin_Candy {
 
 //añade un producto en la máquina en la posicion que se pasa como parametro,
 //si existe en otra posicion actualiza el precio al nuevo valor.
-	private static boolean cambiarProducto(int[] posicion, String[] productos, int[][] tienda, int[] ventas, String producto) {
+	private static void cambiarProducto(int[] posicion, String[] productos, int[][] tienda, int[] ventas,
+			String producto) {
 		int productoAntiguo = cuentaOcurrenciasProducto(tienda, posicion);
 		int index = -1;
 		if (productoAntiguo < 2) {
@@ -215,7 +224,6 @@ public class Samuel_Martin_Candy {
 			index = añadeProductoLista(producto, productos);
 		}
 		añadeTienda(index, tienda, posicion);
-		return true;
 	}
 
 //añade un producto a un slot de la tienda
@@ -247,13 +255,13 @@ public class Samuel_Martin_Candy {
 		ordenaVentas(productosOrd, ventasOrd, preciosOrd);
 		int index = ventas.length;
 		int cuenta = 0;
-		String salida= "";
+		String salida = "";
 		while (index > 0) {
 			index--;
 			if (cuenta < 3 || ventasOrd[index + 1] == ventasOrd[index]) {
 				if (!productosOrd[index].isEmpty() && ventasOrd[index] > 0) {
 					cuenta++;
-					salida+=(String.format("| %-20s | Precio: %2.2f |  Ventas: %3d |\n", productosOrd[index],
+					salida += (String.format("| %-20s | Precio: %2.2f |  Ventas: %3d |\n", productosOrd[index],
 							preciosOrd[index], ventasOrd[index]));
 				}
 			} else {
@@ -261,7 +269,7 @@ public class Samuel_Martin_Candy {
 			}
 		}
 		if (cuenta == 0)
-			salida ="\nNo se ha vendido nada aún!\n";
+			salida = "\nNo se ha vendido nada aún!\n";
 		return salida;
 	}
 
@@ -342,13 +350,15 @@ public class Samuel_Martin_Candy {
 		int[][] existencias = new int[4][4];
 		int[] ventas = new int[16];
 		double cajaTotal = 0;
-		String password ="DAM";
+		String password = "DAM";
 		boolean apagar = false;
+		int[] posicion = new int[2];
 
 		inicializarProductos(productos, ventas, precios, tienda, existencias);
 		sc.useDelimiter("\n");
 		while (!apagar) {
-			System.out.print("\n*** Opciones ***\n\n1. Pedir golosina\n2. Mostrar golosinas\n3. Submenu administrador\n");
+			System.out
+					.print("\n*** Opciones ***\n\n1. Pedir golosina\n2. Mostrar golosinas\n3. Submenu administrador\n");
 			int opcion = 0;
 			try {
 				opcion = sc.nextInt();
@@ -356,149 +366,135 @@ public class Samuel_Martin_Candy {
 				sc.next();
 			}
 			switch (opcion) {
-			case 1: //pedir producto
+			case 1: // pedir producto
 				System.out.print("Introduce la posicion:");
-				int[] posicion = recogePosicion(sc.next());
-				if (posicion[0] >= 0) {
-					double venta = venderProducto(posicion, ventas, tienda, existencias, precios);
-					if (venta > 0) {
-						cajaTotal += venta;
-						System.out.print("\nPuede retirar su producto\n");
-					} else {
-						System.out.print("\nNo hay existencias de ese producto\n");
-					}
+				if (!recogePosicion(sc.next(), posicion)) {
+					System.out.print("\nPosición invalida\n");
+					continue;
 				}
+				if (compruebaExistencias(posicion, existencias) <= 0) {
+					System.out.print("\nNo hay existencias de ese producto\n");
+					continue;
+				}
+				double valorVenta = venderProducto(posicion, ventas, tienda, existencias, precios);
+				cajaTotal += valorVenta;
+				System.out.print("\nPuede retirar su producto\n");
 				break;
-			case 2: //mostrar producto
+			case 2: // mostrar producto
 				System.out.print(mostarProductos(tienda, productos, precios));
 				break;
-			case 3: //menu administracion
+			case 3: // menu administracion
 				System.out.print("\nIntroduzca la contraseña de administracion\n");
 				String pass = sc.next();
-				if (password.equals(pass)) {
-					System.out.print("\nBienvenid@ al panel de administracion:\n");
-					boolean admin = true;
-					while (admin) {
-						System.out.print("\n*** Opciones administrativas***\n\n1. Cambiar contraseña\n");
-						System.out.print("2. Reponer producto\n3. Cambiar precio\n4. Cambiar producto\n");
-						System.out.print("5. Más vendidos\n6. Menos vendidos\n7. Informacion productos\n");
-						System.out.print("8. Ventas totales\n9. Cerrar sesion\n10. Apagar la máquina\n\n");
-						opcion = 0;
-						try {
-							opcion = sc.nextInt();
-						} catch (InputMismatchException e) {
-							sc.next();
-						}
-						switch (opcion) {
-						case 1: //cambiar contraseña
-							System.out.print("\nIntroduzca nueva contraseña\n");
-							String newPass = sc.next();
-							System.out.print("\nRepita nueva contraseña\n");
-							String confPass = sc.next();
-							if (newPass.equals(confPass)) {
-								System.out.print("\nContraseña actualizada\n");
-								password = newPass;
-							} else {
-								System.out.print("\nLas contraseñas no coinciden\n");
-							}
-							break;
-						case 2: //reponer producto
-							System.out.print("Introduce la posicion:");
-							posicion = recogePosicion(sc.next());
-							if (posicion[0] >= 0) {
-								System.out.print("\nIntroduzca la cantidad a reponer\n");
-								System.out.printf("Actualmente queda(n) %d\n",compruebaExistencias(posicion, existencias));
-								try {
-									System.out.print(reponerProducto(posicion, existencias, sc.nextInt()));
-								} catch (InputMismatchException e) {
-									System.out.print("\nRevise los datos\n");
-									sc.next();
-								}
-							} else {
-								System.out.print("\nPosición invalida\n");
-							}
-							break;
-						case 3: //cambiar precio
-							System.out.print("Introduce la posicion:");
-							posicion = recogePosicion(sc.next());
-							if (posicion[0] >= 0) {
-								int index = eligeProducto(posicion, tienda);
-								try {
-									System.out.print("\nIntroduzca el precio de venta.");
-									System.out.printf("Actualmente está a  %.02f€\n", precios[index]);
-									System.out.print(actualizarPrecio(index, precios,sc.nextDouble()));
-								} catch (InputMismatchException e) {
-									System.out.print("\nRevise los datos\n");
-									sc.next();
-								}
-							} else {
-								System.out.print("\nPosición invalida\n");
-							}
-							break;
-						case 4: //cambiar producto
-							System.out.print("Introduce la posicion:");
-							posicion = recogePosicion(sc.next());
-							if (posicion[0] >= 0) {
-								System.out.print("Introduce el nombre del nuevo producto:");
-								int rbIndex = eligeProducto(posicion, tienda);
-								String rbNombre = productos[rbIndex];
-								int rbExistencias = compruebaExistencias(posicion, existencias);
-								int rbVentas = ventas[rbIndex];
-								if (cambiarProducto(posicion, productos, tienda, ventas,sc.next())) {
-									int index = eligeProducto(posicion, tienda);
-									System.out.print("\nIntroduzca el precio de venta.");
-									try {
-										System.out.print(actualizarPrecio(index, precios,sc.nextDouble()));
-										establecerExistencias(posicion,existencias,0);
-										System.out.print("\nIntroduzca la cantidad a reponer\n");
-										System.out.print(reponerProducto(posicion, existencias, sc.nextInt()));
-										System.out.print("\nEl producto se ha actualizado correctamente\n");
-									} catch (InputMismatchException e) {
-										cambiarProducto(posicion, productos, tienda, ventas, rbNombre);
-										index = eligeProducto(posicion, tienda);
-										ventas[index]=rbVentas;
-										reponerProducto(posicion, existencias, rbExistencias);
-										System.out.print("Revise los datos");
-										sc.next();
-										}
-									
-								} else {
-									System.out.print("\nError, revise los datos\n");
-								}
-							} else {
-								System.out.print("\nPosición invalida\n");
-							}
-							break;
-						case 5: //mostrar más vendidos
-							System.out.print(muestraTopVentas(ventas, productos, precios));
-							break;
-						case 6: //mostrar menos vendidos
-							System.out.print(muestraMenosVentas(ventas, productos, precios));
-							break;
-						case 7: //mostrar información de los productos
-							ordenaAlfabetico(ventas, productos, precios, tienda);
-							System.out.print(infoProductos(tienda, existencias, productos, precios, ventas));
-							break;
-						case 8: //Recaudación total
-							System.out.print(String.format("\nVentas totales hasta este momento: %.02f€\n\n", cajaTotal));
-							break;
-						case 9: //Salir menu de administración
-							admin = false;
-							break;
-						case 10: //apagar la máquina
-							admin = false;
-							apagar = true;
-							break;
-						default: //opcion erronea
-							System.out.print("\nHa indicado una opcion incorrecta\n");
-							break;
-						}
-					}
-				} else {
+				if (!password.equals(pass)) {
 					System.out.print("\nLa contraseña es incorrecta\n");
+				} else {
+				System.out.print("\nBienvenid@ al panel de administracion:\n");
+				boolean admin = true;
+				while (admin) {
+					System.out.print("\n*** Opciones administrativas***\n\n1. Cambiar contraseña\n");
+					System.out.print("2. Reponer producto\n3. Cambiar precio\n4. Cambiar producto\n");
+					System.out.print("5. Más vendidos\n6. Menos vendidos\n7. Informacion productos\n");
+					System.out.print("8. Ventas totales\n9. Cerrar sesion\n10. Apagar la máquina\n\n");
+					opcion = 0;
+					try {
+						opcion = sc.nextInt();
+					} catch (InputMismatchException e) {
+						sc.next();
+					}
+					switch (opcion) {
+					case 1: // cambiar contraseña
+						System.out.print("\nIntroduzca nueva contraseña\n");
+						String newPass = sc.next();
+						System.out.print("\nRepita nueva contraseña\n");
+						String confPass = sc.next();
+						if (newPass.equals(confPass)) {
+							System.out.print("\nContraseña actualizada\n");
+							password = newPass;
+						} else {
+							System.out.print("\nLas contraseñas no coinciden\n");
+						}
+						break;
+					case 2: // reponer producto
+						System.out.print("Introduce la posicion:");
+						if (recogePosicion(sc.next(), posicion)) {
+							System.out.print("\nIntroduzca la cantidad a reponer\n");
+							System.out.printf("Actualmente queda(n) %d\n", compruebaExistencias(posicion, existencias));
+							try {
+								System.out.print(reponerProducto(posicion, existencias, sc.nextInt()));
+							} catch (InputMismatchException e) {
+								System.out.print("\nRevise los datos\n");
+								sc.next();
+							}
+						} else {
+							System.out.print("\nPosición invalida\n");
+						}
+						break;
+					case 3: // cambiar precio
+						System.out.print("Introduce la posicion:");
+						if (recogePosicion(sc.next(), posicion)) {
+							int index = eligeProducto(posicion, tienda);
+							try {
+								System.out.print("\nIntroduzca el precio de venta.");
+								System.out.printf("Actualmente está a  %.02f€\n", precios[index]);
+								System.out.print(actualizarPrecio(index, precios, sc.nextDouble()));
+							} catch (InputMismatchException e) {
+								System.out.print("\nRevise los datos\n");
+								sc.next();
+							}
+						} else {
+							System.out.print("\nPosición invalida\n");
+						}
+						break;
+					case 4: // cambiar producto
+						System.out.print("Introduce la posicion:");
+						if (recogePosicion(sc.next(), posicion)) {
+							System.out.print("Introduce el nombre del nuevo producto:");
+							cambiarProducto(posicion, productos, tienda, ventas, sc.next());
+							int index = eligeProducto(posicion, tienda);
+							System.out.print("\nIntroduzca el precio de venta.");
+							try {
+								System.out.print(actualizarPrecio(index, precios, sc.nextDouble()));
+								reseteaExistencias(posicion, existencias);
+								System.out.print("\nIntroduzca la cantidad a reponer\n");
+								System.out.print(reponerProducto(posicion, existencias, sc.nextInt()));
+								System.out.print("\nEl producto se ha actualizado correctamente\n");
+							} catch (InputMismatchException e) {
+								System.out.print("Revise los datos");
+								sc.next();
+							}
+						} else {
+							System.out.print("\nPosición invalida\n");
+						}
+						break;
+					case 5: // mostrar más vendidos
+						System.out.print(muestraTopVentas(ventas, productos, precios));
+						break;
+					case 6: // mostrar menos vendidos
+						System.out.print(muestraMenosVentas(ventas, productos, precios));
+						break;
+					case 7: // mostrar información de los productos
+						ordenaAlfabetico(ventas, productos, precios, tienda);
+						System.out.print(infoProductos(tienda, existencias, productos, precios, ventas));
+						break;
+					case 8: // Recaudación total
+						System.out.print(String.format("\nVentas totales hasta este momento: %.02f€\n\n", cajaTotal));
+						break;
+					case 9: // Salir menu de administración
+						admin = false;
+						break;
+					case 10: // apagar la máquina
+						admin = false;
+						apagar = true;
+						break;
+					default: // opcion erronea
+						System.out.print("\nHa indicado una opcion incorrecta\n");
+						break;
+					}
+				}
 				}
 				break;
-			default: //opcion erronea
+			default: // opcion erronea
 				System.out.print("\nHa indicado una opcion incorrecta\n");
 				break;
 			}
